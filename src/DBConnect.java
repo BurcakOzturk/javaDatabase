1d3657cf24bf90ebc765d4f3708e65c09133e5b4
Burçak ÖZTÜRK <159647466+BurcakOzturk@users.noreply.github.com>
2024-04-17 01:15:39 +0300

    ilk yayım

--- src/DBConnect.java
+++ src/DBConnect.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class DBConnect {
+
+import java.sql.*;
+
+public class DBConnect {
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/dvdrental";
+    public static final String DB_USERNAME = "postgres";
+    public static final String DB_PASSWORD = "burcak";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement st = null;
+
+        try {
+
+            conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            st = conn.createStatement();
+            ResultSet resultSet = st.executeQuery("SELECT * FROM employee");
+
+            while (resultSet.next()) {
+                System.out.println("ID : " + resultSet.getInt("id"));
+                System.out.println("Adı : " + resultSet.getString("name"));
+                System.out.println("Pozisyonu : " + resultSet.getString("position"));
+                System.out.println("Maaşı : " + resultSet.getInt("salary"));
+                System.out.println("#################");
+            }
+
+        }
+        catch (SQLException ex) {
+
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+
 }
